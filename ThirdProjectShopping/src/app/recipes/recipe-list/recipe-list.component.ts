99a3b9f55d0ffd2c0e5a66c1e44bf448e2965cce
Theerthesh <theertheshpart@gmail.com>
2024-03-05 11:55:24 +0530

    added sample recipe sample list and design

--- ThirdProjectShopping/src/app/recipes/recipe-list/recipe-list.component.ts
+++ ThirdProjectShopping/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Recipe } from '../recipe.module';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrl: './recipe-list.component.css'
 })
 export class RecipeListComponent {
+
+  recipes:Recipe[]=[
+    new Recipe('A test Recipe','this is simple a test', 'https://www.feastingathome.com/wp-content/uploads/2020/06/larb-recipe-20-1000x1477.jpg')
+  ];
 
 }
