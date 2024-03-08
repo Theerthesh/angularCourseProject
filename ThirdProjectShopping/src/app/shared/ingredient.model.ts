d43ae08a990783c1b4d55a3ecd822d553ec7b1cd
Theerthesh <theertheshpart@gmail.com>
2024-03-08 11:57:08 +0530

    created ingredient model

---
+++ ThirdProjectShopping/src/app/shared/ingredient.model.ts
@@ -0,0 +1,12 @@
+export class Ingredient{
+// public name:string;
+// public amount:number;
+
+constructor(public name:string,public amount:number){
+    // this.name = name;
+    // this.amount = amount;
+}
+
+// adding public in constractor is =  to creating and assigning to variable is same
+}
+
