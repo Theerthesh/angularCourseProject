f53db2e2cd96cb382d627e67be4dd345ad58b664
Theerthesh <theertheshpart@gmail.com>
2024-03-26 14:18:01 +0530

    commit

--- ThirdProjectShopping/src/app/app.component.ts
+++ ThirdProjectShopping/src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ThirdProjectShopping';
+headerSelected = 'recipe';
+
+  onHeaderSelected(feature:string){
+this.headerSelected = feature;
+  }
 }
