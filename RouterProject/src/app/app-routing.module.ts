8afa35e43a0bb25c22d1f1b169d9c27f25c465cf
Theerthesh <theertheshpart@gmail.com>
2024-05-29 12:59:14 +0530

    added router feature to UI

--- RouterProject/src/app/app-routing.module.ts
+++ RouterProject/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UsersComponent } from './users/users.component';
+import { HomeComponent } from './home/home.component';
+import { ServersComponent } from './servers/servers.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component:HomeComponent},
+  {path:'services', component:ServersComponent},
+  {path:'users', component:UsersComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
