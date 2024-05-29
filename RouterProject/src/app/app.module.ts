1ccf4d2ee2b962d80ac072d67f3f028cdc522287
Theerthesh <theertheshpart@gmail.com>
2024-05-29 12:35:56 +0530

    created required components

--- RouterProject/src/app/app.module.ts
+++ RouterProject/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ServersComponent } from './servers/servers.component';
+import { UsersComponent } from './users/users.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ServersComponent,
+    UsersComponent
   ],
   imports: [
     BrowserModule,
