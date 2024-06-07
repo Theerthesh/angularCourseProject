b02970655dd58e7dd2441be11ef19d9bf403a497
Theerthesh <theertheshpart@gmail.com>
2024-06-07 14:00:57 +0530

    added routes for recipes and shopping list

--- ThirdProjectShopping/src/app/app-routing.module.ts
+++ ThirdProjectShopping/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RecipesComponent } from './recipes/recipes.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',redirectTo:'receips',pathMatch:'full'},
+  {path:'receips',component:RecipesComponent},
+  {path:'shopping-list',component:ShoppingListComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
